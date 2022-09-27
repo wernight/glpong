f11e3ff1ed468a97540ff1a8ae8cf6b0cf867050
Werner Beroux <werner@beroux.com>
2022-09-27 19:38:07 +0200

    Generate AppImage.

--- Dockerfile
+++ Dockerfile
@@ -13,5 +13,22 @@ RUN set -x \
  && apt clean \
  && rm -rf /var/lib/apt/lists/*
 
+# https://docs.appimage.org/packaging-guide/from-source/linuxdeploy-user-guide.html
+# https://github.com/AppImage/AppImageKit
+ARG LINUXDEPLOY_APPIMAGE_URL=https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+RUN set -eux; \
+    curl -Lo /tmp/linuxdeploy-x86_64.AppImage ${LINUXDEPLOY_APPIMAGE_URL}; \
+    chmod +x /tmp/linuxdeploy-x86_64.AppImage; \
+    mkdir /opt/linuxdeploy; \
+    cd /opt/linuxdeploy; \
+    /tmp/linuxdeploy-x86_64.AppImage --appimage-extract; \
+    ln -s /opt/linuxdeploy/squashfs-root/AppRun /usr/local/bin/linuxdeploy; \
+    apt-get update; \
+    apt-get install -y \
+        file \
+        zsync; \
+    apt-get clean; \
+    rm -rf /var/lib/apt/lists/*
+
 WORKDIR /code
 CMD ["/code/make.sh"]
--- make.sh
+++ make.sh
@@ -1,5 +1,32 @@
 #!/bin/bash -ex
+CODEDIR=/code
+
+BUILDDIR=/tmp/glpong
 mkdir -p /tmp/glpong
-cd /tmp/glpong
-cmake -DCMAKE_INSTALL_PREFIX=/code/bin/linux /code
-make install
+cmake \
+    -B $BUILDDIR \
+    -S $CODEDIR
+cmake --build $BUILDDIR
+
+# Generate the AppDir folder content.
+APPDIR=/tmp/GLPong.AppDir
+rm -rf ${APPDIR} || true
+mkdir -p ${APPDIR}/usr/bin/
+cp /code/res/*.png ${APPDIR}/usr/bin/
+cp $BUILDDIR/glpong ${APPDIR}/usr/bin/
+
+# Generate the AppImage.
+# https://docs.appimage.org/packaging-guide/from-source/linuxdeploy-user-guide.html
+# https://docs.appimage.org/packaging-guide/manual.html
+# The additional dependencies can only be tested by running something like:
+linuxdeploy \
+    --appdir=${APPDIR} \
+    --executable=${BUILDDIR}/glpong \
+    --desktop-file=${CODEDIR}/res/GLPong.desktop \
+    --icon-file=${CODEDIR}/res/GLPong.png \
+    --library=/usr/lib/x86_64-linux-gnu/libOpenGL.so.0 \
+    --library=/usr/lib/x86_64-linux-gnu/libGLdispatch.so.0 \
+    --output \
+    appimage
+
+mv *.AppImage /code/bin/
